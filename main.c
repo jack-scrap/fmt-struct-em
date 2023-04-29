2e18eebd774d67ab202c1e9f89aa46d2674bad4e
Jack <jackhasakeyboard@gmail.com>
2023-04-29 04:29:48 -0600

    Output em dash

--- main.c
+++ main.c
@@ -1,3 +1,17 @@
+#include <stdio.h>
+
+void err(char* msg) {
+	fprintf(stderr, "Error: %s\n", msg);
+}
+
 int main(int argc, char* argv[]) {
+	if (argc != 1) {
+		err("Wrong number of arguments");
+
+		return 1;
+	}
+
+	printf("--");
+
 	return 0;
 }
